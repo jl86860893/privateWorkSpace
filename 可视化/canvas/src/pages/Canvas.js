89b128981a5cd0569d4db20f99f0f1095488d7e8
lu.jiang_U+ <lu.jiang@united-imaging.com>
2020-07-23 20:35:41 +0800

    1

--- 可视化/canvas/src/pages/Canvas.js
+++ 可视化/canvas/src/pages/Canvas.js
@@ -2,21 +2,31 @@ import React, { Component } from 'react';
 
 export default class Canvas extends Component {
   componentDidMount() {
+    this.draw(500, 500, 75, 80 )
+  }
+
+  // stroke,fill
+  draw(x1, y1, r, percent) {
     const context = this.canvas.getContext('2d');
-    const rectSize = [100, 100];
-    context.fillStyle = 'red';
-    context.save();
-    context.translate(-0.5 * rectSize[0], -0.5 * rectSize[1])
-    context.beginPath()
-    context.rect(0.5 * this.canvas.width, 0.5 * this.canvas.height, ...rectSize)
-    context.fill()
-    context.restore()
+
+    context.beginPath();
+    context.arc(x1, y1, 100, 0, 2 * Math.PI, false);
+    context.lineWidth = 20;
+    context.strokeStyle = 'red';
+    context.stroke();
+
+    const diffAngle = percent / 100 * Math.PI * 2;
+    context.beginPath();
+    context.lineWidth = 22;
+    context.arc(x1, y1, 100, 0, diffAngle, false);
+    context.strokeStyle = 'green';
+    context.stroke()
   }
 
   render() {
     return (
-      <div style={{ width: '512px', height: '512px' }}>
-        <canvas ref={node => this.canvas = node} width="512" height="512"></canvas>
+      <div style={{ backgroundColor: 'grey' }}>
+        <canvas ref={node => this.canvas = node} width="800" height="800"></canvas>
       </div>
     )
   }
