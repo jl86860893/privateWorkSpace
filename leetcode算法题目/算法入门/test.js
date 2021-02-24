77e42b5adb8e9741e041da529d6b0d59e00ee3e2
jl86860893 <291509080@qq.com>
2021-02-24 15:16:34 +0800

    1

--- leetcode算法题目/算法入门/test.js
+++ leetcode算法题目/算法入门/test.js
@@ -1,43 +1,51 @@
-function quickSort(arr) {
-    process(arr, -1, 0, arr.length)
-  }
-  
-  function process(depart, m, i, n) {
-    let radomRight = Math.random() * arr.length;
-    swap(arr, radomRight, arr.length-1)
-    const nextborder = sortHL(arr, m, i, n-1, arr[radomRight])
-    const {L,R} = nextborder;
-    swap(arr, L+1, n-1)
-    process(arr, m, m+1, L+1)
-    process(arr, R-1, R, n)
-  }
-  
-  function sortHL(arr, m, i, n, target) {
-        if (m === n || i >= n) {
-          return {
-              L:m,
-              R:n,
-          };
-      }
-      if (arr[i] < target) {
-          swap(arr, m+1, i)
-          i += 1
-          m += 1;
-      } else if(arr[i] === target){
-          i+=1
-      } else {
-          swap(arr, n-1, i)
-          n -= 1;
-      }
-      process(arr, m, i, n)    
-  }
-  
-  function swap(arr, a, b) {
-      let temp = arr[a];
-      arr[a] = arr[b];
-      arr[b] = temp;
-  }
-  
-  let arr = [3,6,4,5,7,5,2,0]
-  quickSort(arr)
-  console.log(arr)
+function heapSort(arr) {
+    if (arr.length < 2) {
+        return
+    }
+    for(var i = 0; i < arr.length; i++) {
+        heapInsert(arr, i)
+        console.log(arr)
+    }
+
+    // for(let i = arr.length - 1; i >= 0; i--) {
+    //     heapify(arr, i, arr.length)
+    // }
+    let heapSize = arr.length;
+    swap(arr, 0, --heapSize)
+    while(heapSize > 0) {
+        heapify(arr, 0, heapSize)
+        heapSize -= 1;
+        swap(arr, 0, heapSize)
+    }
+}
+
+function heapInsert(arr, index) {
+    while(arr[index] > arr[Math.floor((index - 1) / 2)]) {
+        swap(arr, index, Math.floor((index - 1) / 2))
+        index = Math.floor((index - 1) / 2);
+    }
+}
+
+function heapify(arr, start, heapSize) {
+    let left = start * 2 + 1;
+    while(left < heapSize) {
+        let largest = left + 1 < heapSize && arr[left] < arr[left + 1] ? left + 1: left;
+        largest = arr[largest] > arr[start] ? largest : start;
+        if (largest === start) {
+            break;
+        }
+        swap(arr, start, largest);
+        start = largest;
+        left = start * 2 + 1;
+    }
+}
+
+function swap(arr, a, b) {
+    let temp = arr[a]
+    arr[a]= arr[b]
+    arr[b] = temp;
+}
+
+let arr = [3,7,4,5,21,0]
+heapSort(arr)
+console.log(arr)
